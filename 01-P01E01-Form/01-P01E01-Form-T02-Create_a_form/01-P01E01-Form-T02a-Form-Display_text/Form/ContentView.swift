8339bf060f27ebb0f922866da3f205bc4d4a1be2
ignasiperez <ignasiperez@gmail.com>
2023-12-12 13:37:51 +0100

    r – Change name of folders

---
+++ 01-P01E01-Form/01-P01E01-Form-T02-Create_a_form/01-P01E01-Form-T02a-Form-Display_text/Form/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Form
+//
+//  Created by Ignasi Perez-Valls on 23/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01-P01E01-Form/01-P01E01-Form-T02-Create_a_form/01-P01E01-Form-T02b-Form-Display_text_and_sections/Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Ignasi Perez-Valls on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
