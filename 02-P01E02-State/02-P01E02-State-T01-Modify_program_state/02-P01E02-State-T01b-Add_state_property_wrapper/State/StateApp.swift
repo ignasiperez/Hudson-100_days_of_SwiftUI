58fea21eb8554c0b934b409b8034f8f5474d6a46
ignasiperez <ignasiperez@gmail.com>
2023-12-14 12:19:55 +0100

    f - 02-P01E02-Task01. Modify program state

---
+++ 02-P01E02-State/02-P01E02-State-T01-Modify_program_state/02-P01E02-State-T01b-Add_state_property_wrapper/State/StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateApp.swift
+//  State
+//
+//  Created by Ignasi Perez-Valls on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
