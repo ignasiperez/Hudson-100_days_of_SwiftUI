dd3a62ac95a36bc63cacc0d06d101c26d9f0d336
ignasiperez <ignasiperez@gmail.com>
2023-12-14 12:32:42 +0100

    e - 03-P01E03-Task00. New Xcode project – BindingState

---
+++ 03-P01E03-BindingState/03-P01E03-T00-New_Xcode_Project/BindingState/BindingStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingStateApp.swift
+//  BindingState
+//
+//  Created by Ignasi Perez-Valls on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BindingStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
