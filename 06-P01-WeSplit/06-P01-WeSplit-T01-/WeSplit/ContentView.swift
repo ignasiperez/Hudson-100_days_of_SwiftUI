86d670b8f686dbe6052e6b831b0d3b0df335d30f
ignasiperez <ignasiperez@gmail.com>
2023-12-23 09:46:43 +0100

    f - 06-P01-Task01. Add checkAmount TextField with currency format

---
+++ 06-P01-WeSplit/06-P01-WeSplit-T01-/WeSplit/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ignasi Perez-Valls on 21/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section {
+//              TextField("Amount", text: $checkAmount)
+//              TextField(
+//                  "Amount",
+//                  value: $checkAmount,
+//                  format: .currency(code: "USD")
+//              )
+                TextField(
+                    "Amount",
+                    value: $checkAmount,
+                    format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                )
+            }
+        }
+    } // body
+    
+} // ContentView
+
+#Preview {
+    ContentView()
+}
