6ea46a790133ff471acb7c2ac15e3f6610663784
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-12 17:41:17 +0200

    • f 02-P02 Create WeSplit project

---
+++ 02-P02-WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ignasi Perez-Valls on 12/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+} // ContentView
+
+#Preview {
+    ContentView()
+}
---
+++ 02-P02-WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ignasi Perez-Valls on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
