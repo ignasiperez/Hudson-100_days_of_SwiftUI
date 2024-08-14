75319546816df0d021e0724ce0487e888eb6cfa4
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-14 19:12:38 +0200

    • f Fill in .secondary semantic color

---
+++ 01-L01-WeSplit-Overview/WeSplit-Overview/ZStack_Color_SemanticColors.swift
@@ -0,0 +1,21 @@
+//
+//  ZStack_Color_SemanticColors.swift
+//  WeSplit-Overview
+//
+//  Created by Ignasi Perez-Valls on 14/8/24.
+//
+
+import SwiftUI
+
+struct ZStack_Color_SemanticColors: View {
+    var body: some View {
+        ZStack {
+            Color.secondary
+            Text("Your content")
+        }
+    }
+}
+
+#Preview {
+    ZStack_Color_SemanticColors()
+}
