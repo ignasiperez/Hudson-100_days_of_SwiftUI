b2f14478fbfd430bc96b5aef6e1e10842e45a2e4
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-16 13:13:34 +0200

    • f Fill in RGB custom color

---
+++ 03-L02-GuessTheFlag-Overview/GuessTheFlag-Overview/ZStack_Color_RGBCostumeValues.swift
@@ -0,0 +1,20 @@
+//
+//  ZStack_Color_RGBCostumeValues.swift
+//  GuessTheFlag-Overview
+//
+//  Created by Ignasi Perez-Valls on 16/8/24.
+//
+
+import SwiftUI
+
+struct ZStack_Color_RGBCostumeValues: View {
+    var body: some View {
+        ZStack {
+            Color(red: 1, green: 0.8, blue: 1)
+        }
+    }
+}
+
+#Preview {
+    ZStack_Color_RGBCostumeValues()
+}
