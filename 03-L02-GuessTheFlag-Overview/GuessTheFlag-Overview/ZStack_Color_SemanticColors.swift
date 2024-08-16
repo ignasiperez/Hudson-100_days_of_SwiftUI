cbb73a723ae93781cc876186a9b2981b124072f7
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-16 12:55:35 +0200

    • f Fill in .secondary semantic color

---
+++ 03-L02-GuessTheFlag-Overview/GuessTheFlag-Overview/ZStack_Color_SemanticColors.swift
@@ -0,0 +1,21 @@
+//
+//  ZStack_Color_SemanticColors.swift
+//  GuessTheFlag-Overview
+//
+//  Created by Ignasi Perez-Valls on 16/8/24.
+//
+
+import SwiftUI
+
+struct ZStack_Color_SemanticColors: View {
+    var body: some View {
+        ZStack {
+            Color.secondary
+            Text("Your content")
+        }
+    }
+}
+
+#Preview {
+    ZStack_Color_SemanticColors()
+}
