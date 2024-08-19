0c01800d3e5141d2134a465a5409f2e874980bbb
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-19 14:20:45 +0200

    • f 01-L01 Create SwiftUI_Projects_Overviews project

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 19/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/SwiftUI_Projects_OverviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Projects_OverviewsApp.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 19/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Projects_OverviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
