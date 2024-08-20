95a56f60a497c5707aad18b65dcd213ce4aee343
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-20 18:41:10 +0200

    • f Embed a text in a §ZStack with an .orange §background on ZStack

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/GuessTheFlag/ZStack_background1.swift
@@ -0,0 +1,21 @@
+//
+//  ZStack_background1.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 20/8/24.
+//
+
+import SwiftUI
+
+struct ZStack_background1: View {
+    var body: some View {
+        ZStack {
+            Text("Your content")
+        }
+        .background(.orange)
+    }
+}
+
+#Preview {
+    ZStack_background1()
+}
