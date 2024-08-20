65ccf742a89adf970b11a66b0973d7e5be6320ee
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-20 18:24:00 +0200

    • Add one §Spacer at the beginning and two Spacers at the end of a §VStack

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/GuessTheFlag/VStack_Spacers.swift
@@ -0,0 +1,25 @@
+//
+//  VStack_Spacers.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 20/8/24.
+//
+
+import SwiftUI
+
+struct VStack_Spacers: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("First")
+            Text("Second")
+            Text("Third")
+            Spacer()
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    VStack_Spacers()
+}
