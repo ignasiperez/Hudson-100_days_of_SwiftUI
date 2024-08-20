5a9d440f40766f7f5a739e13c9b66ffc33641abe
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-20 18:10:07 +0200

    • f Add a §.leading §alignment to a §VStack

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/GuessTheFlag/VStack_alignment.swift
@@ -0,0 +1,21 @@
+//
+//  VStack_alignment.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 20/8/24.
+//
+
+import SwiftUI
+
+struct VStack_alignment: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello, World!")
+            Text("This is inside a stack")
+        }
+    }
+}
+
+#Preview {
+    VStack_alignment()
+}
