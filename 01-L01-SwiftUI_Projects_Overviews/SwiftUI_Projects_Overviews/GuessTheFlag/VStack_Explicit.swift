8948987c90432c476d56d09547db527b6373536f
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-20 17:55:50 +0200

    • f Place two text views in an implicit VStack

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/GuessTheFlag/VStack_Explicit.swift
@@ -0,0 +1,21 @@
+//
+//  VStack_Explicit.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 20/8/24.
+//
+
+import SwiftUI
+
+struct VStack_Explicit: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+            Text("This is inside a stack")
+        }
+    }
+}
+
+#Preview {
+    VStack_Explicit()
+}
