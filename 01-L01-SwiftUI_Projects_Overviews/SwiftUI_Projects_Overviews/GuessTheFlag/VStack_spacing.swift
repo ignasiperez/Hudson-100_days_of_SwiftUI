d2b8806a192d54f4984e38f7d46120e498f8de1d
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-20 18:03:51 +0200

    • f Add a vertical §spacing to a §VStack

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/GuessTheFlag/VStack_spacing.swift
@@ -0,0 +1,22 @@
+//
+//  VStack_spacing.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 20/8/24.
+//
+// §VStack(aligning:§spacing:content:)
+
+import SwiftUI
+
+struct VStack_spacing: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Hello, World!")
+            Text("This is inside a stack")
+        }
+    }
+}
+
+#Preview {
+    VStack_spacing()
+}
