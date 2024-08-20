55efcf88a4fce82cd0e0fb4e32ce554a3262f673
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-20 18:47:04 +0200

    • f Fill in orange §Color the whole area

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/GuessTheFlag/ZStack_Color_wholeArea.swift
@@ -0,0 +1,21 @@
+//
+//  ZStack_Color_wholeArea.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 20/8/24.
+//
+
+import SwiftUI
+
+struct ZStack_Color_wholeArea: View {
+    var body: some View {
+        ZStack {
+            Color.orange
+            Text("Your content")
+        }
+    }
+}
+
+#Preview {
+    ZStack_Color_wholeArea()
+}
