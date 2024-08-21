e2b1398af63a562fcbb07b4f540c08400d14ce25
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-21 11:16:13 +0200

    • f Fill in orange a specific size §frame with minimum and maximum widths and heights

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/GuessTheFlag/ZStack_Color_frame_minWidth.swift
@@ -0,0 +1,26 @@
+//
+//  Stack_Color_minWidth.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 21/8/24.
+//
+
+import SwiftUI
+
+struct ZStack_Color_frame_minWidth: View {
+    var body: some View {
+        ZStack {
+            Color.orange
+                .frame(
+                    minWidth: 200,
+                    maxWidth: .infinity,
+                    maxHeight: 200
+                )
+            Text("Your content")
+        }
+    }
+}
+
+#Preview {
+    ZStack_Color_frame_minWidth()
+}
