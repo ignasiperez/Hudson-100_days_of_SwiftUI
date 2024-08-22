924be406973656722a597b96a5748802c6d4141f
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-22 11:20:07 +0200

    • f Add a LinearGradient providing gradient stops

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/GuessTheFlag/LinearGradient_stops1.swift
@@ -0,0 +1,29 @@
+//
+//  LinearGradient_stops1.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 22/8/24.
+//
+// §LinearGradient(§stops:startPoint:endPoint:)
+//
+// A gradient stops let you specify both a color and how far along the gradient the color should be used.
+// For example, we could specify that our gradient should be white from the start up to 45% of the available space, then black from 55% of the available space onwards.
+
+import SwiftUI
+
+struct LinearGradient_stops1: View {
+    var body: some View {
+        LinearGradient(
+            stops: [
+                Gradient.Stop(color: .white, location: 0.45),
+                Gradient.Stop(color: .black, location: 0.55)
+                   ],
+            startPoint: .top,
+            endPoint: .bottom
+        )
+    }
+}
+
+#Preview {
+    LinearGradient_stops1()
+}
