e170bb688a38393135d9a8d6a3deb7196d4b6642
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-26 18:45:10 +0200

    • f Create a State that tracks whether our alert is showing

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/GuessTheFlag/Button_alert.swift
@@ -0,0 +1,20 @@
+//
+//  Button_alert.swift
+//  SwiftUI_Projects_Overviews
+//
+//  Created by Ignasi Perez-Valls on 26/8/24.
+//
+
+import SwiftUI
+
+struct Button_alert: View {
+    @State private var showingAlert = false
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Button_alert()
+}
