bcd790b3374c4f45aa7932c8aa2e0acb5b7d2cb6
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-27 17:46:14 +0200

    • f Lay out two text views "Tap the flag of" and "§random 0...2" in a vertical stack

--- 03-P02-GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 03-P02-GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -2,20 +2,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = [
+        "Estonia", "France", "Germany", "Ireland",
+        "Italy", "Nigeria", "Poland", "Spain",
+        "UK", "Ukraine", "US"
+    ]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        var countries = [
-            "Estonia", "France", "Germany", "Ireland",
-            "Italy", "Nigeria", "Poland", "Spain",
-            "UK", "Ukraine", "US"
-        ]
-        var correctAnswer = Int.random(in: 0...2)
-        
         VStack {
-            Text("Hello, world!")
+            Text("Tap the flag of")
+            Text(countries[correctAnswer])
         }
-        .padding()
     }
-}
+} // ContentView
 
 #Preview {
     ContentView()
