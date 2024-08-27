ff7f5253ceb9a87161c69172525b4e0f21292b19
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-08-27 13:35:22 +0200

    • f 03-P02 Create GuessTheFlag project

---
+++ 03-P02-GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ignasi Perez-Valls on 27/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
