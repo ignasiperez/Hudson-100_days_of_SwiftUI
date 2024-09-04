97e31f7696770ef0f386d84d0a7bb890693ae78e
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-04 18:54:23 +0200

    • f Apply to a §Text view .padding then a .background color, then more .padding and a different .background color

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/Text_padding_background_padding_background.swift
@@ -0,0 +1,20 @@
+
+import SwiftUI
+
+struct Text_padding_background_padding_background: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+            .background(.orange)
+            .padding()
+            .background(.brown)
+            .padding()
+            .background(.pink)
+            .padding()
+            .background(.indigo)
+    }
+}
+
+#Preview {
+    Text_padding_background_padding_background()
+}
