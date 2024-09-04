6745c3710cdb7ebe1f98c3001de85d53c2edf4b4
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-04 19:34:33 +0200

    • Substitute §some View for §Text

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/OpaqueReturnTypes_body_Text_Text.swift
@@ -0,0 +1,15 @@
+//
+// §opaque return types
+//
+
+import SwiftUI
+
+struct OpaqueReturnTypes_body_Text_Text: View {
+    var body: Text {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    OpaqueReturnTypes_body_Text_Text()
+}
