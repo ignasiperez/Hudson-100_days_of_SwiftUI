8b45231c18c48aa4a0386609a25c90fe6cfc014b
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-05 14:07:02 +0200

    • f Add a .font regular modifier to a Text view

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/VStack_font_Text_font.swift
@@ -0,0 +1,19 @@
+
+import SwiftUI
+
+struct VStack_font_Text_font: View {
+    var body: some View {
+        VStack {
+            Text("Gryffindor")
+                .font(.largeTitle)
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+            Text("Slytherin")
+        }
+        .font(.title)
+    }
+}
+
+#Preview {
+    VStack_font_Text_font()
+}
