54ab4b5e5dba319325df6e8824c97089a2e4e991
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-05 11:23:51 +0200

    • f Change a Button's .foregroundStyle with a ternary conditional operator

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/Button_foregroundStyle_conditionalModifiers.swift
@@ -0,0 +1,18 @@
+
+import SwiftUI
+
+struct Button_foregroundStyle_conditionalModifiers: View {
+    @State private var useRedText = false
+    
+    var body: some View {
+        Button("Hello, World!") {
+            // flip the Boolean between true and false
+            useRedText.toggle()
+        }
+        .foregroundStyle(useRedText ? .red : .blue)
+    }
+}
+
+#Preview {
+    Button_foregroundStyle_conditionalModifiers()
+}
