e20da36dbd1288031ab65caac7242f094a6fc48a
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-06 12:40:47 +0200

    • f Add a §ViewBuilder attribute to a #computed property of §some View with two Text views inside

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/ViewBuilder_GroupAsComputedProperty.swift
@@ -0,0 +1,20 @@
+//
+// Add a @ViewBuilder attribute to a #computed property of §some View with two Text views inside
+//
+
+import SwiftUI
+
+struct ViewBuilder_GroupAsComputedProperty: View {
+    @ViewBuilder var spells: some View {
+        Text("Lumos")
+        Text("Obliviate")
+    }
+    
+    var body: some View {
+        spells
+    }
+}
+
+#Preview {
+    ViewBuilder_GroupAsComputedProperty()
+}
