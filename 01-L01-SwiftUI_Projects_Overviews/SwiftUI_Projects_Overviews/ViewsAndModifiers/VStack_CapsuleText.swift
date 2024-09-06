f3ed7c1b586981a2a4305ccbc9fc360983bd51b6
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-06 13:22:21 +0200

    • f Use two CapsuleText #costum views that encapsulate many styling modifiers

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/VStack_CapsuleText.swift
@@ -0,0 +1,31 @@
+//
+// Use two CapsuleText #costum views that encapsulate many styling modifiers
+//
+
+import SwiftUI
+
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+struct VStack_CapsuleText: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+            CapsuleText(text: "Second")
+        }
+    }
+}
+
+#Preview {
+    VStack_CapsuleText()
+}
