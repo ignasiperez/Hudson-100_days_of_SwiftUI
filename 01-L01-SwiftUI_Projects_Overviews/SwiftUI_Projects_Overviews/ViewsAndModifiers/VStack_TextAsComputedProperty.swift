bbde0708048251233511947ddc459f4bb1fdf931
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-06 12:19:24 +0200

    • f Modify one Text view as #computed property inside a VStack

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/VStack_TextAsComputedProperty.swift
@@ -0,0 +1,26 @@
+//
+// Modify one Text view as #computed property inside a VStack
+//
+
+import SwiftUI
+
+struct VStack_TextAsComputedProperty: View {
+    var motto1: some View {
+        Text("Draco dormiens")
+    }
+    
+    let motto2 = Text("Nunquam titillandus")
+
+    var body: some View {
+        VStack {
+            motto1
+                .foregroundStyle(.red)
+            motto2
+                .foregroundStyle(.blue)
+        }
+    }
+}
+
+#Preview {
+    VStack_TextAsComputedProperty()
+}
