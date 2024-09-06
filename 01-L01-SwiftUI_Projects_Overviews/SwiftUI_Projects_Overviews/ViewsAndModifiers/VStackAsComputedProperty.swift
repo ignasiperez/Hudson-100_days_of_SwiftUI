27e95282abf472a3ae9d7eee184203fffa533ec6
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-06 12:28:08 +0200

    • f Create a #computed property of §some View with a §VStack and two Text views inside

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/VStackAsComputedProperty.swift
@@ -0,0 +1,22 @@
+//
+// Create a #computed property of §some View with a §VStack and two Text views inside
+//
+
+import SwiftUI
+
+struct VStackAsComputedProperty: View {
+    var spells: some View {
+        VStack {
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
+    
+    var body: some View {
+        spells
+    }
+}
+
+#Preview {
+    VStackAsComputedProperty()
+}
