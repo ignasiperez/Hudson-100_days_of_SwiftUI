a5a70c9b58cf8d3011d66c6f43cfde1c944f20a0
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-10 17:29:06 +0200

    • r Restorate code that had been deleted by mistake

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/ViewModifier_Title.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/ViewsAndModifiers/ViewModifier_Title.swift
@@ -5,6 +5,23 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
 struct ViewModifier_Title: View {
     var body: some View {
         Text("Hello, World!")
