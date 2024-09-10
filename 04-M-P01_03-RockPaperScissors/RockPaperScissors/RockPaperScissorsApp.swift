69dc8d2f7e46930b67966ddaff5925f262eb8bdd
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-10 17:58:56 +0200

    • m 04-M-P01_03 Create RockPaperScissors milestone project

---
+++ 04-M-P01_03-RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Ignasi Perez-Valls on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
