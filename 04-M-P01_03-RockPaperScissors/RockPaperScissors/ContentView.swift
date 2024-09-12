8aaa4c6ffa8f359d7996313b0acc95da3cf726ca
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-12 12:25:49 +0200

    • m Create a @State property to store the app’s current choice (currentChoice)

--- 04-M-P01_03-RockPaperScissors/RockPaperScissors/ContentView.swift
+++ 04-M-P01_03-RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -4,6 +4,8 @@ import SwiftUI
 struct ContentView: View {
     private let moves = ["rock", "paper", "scissors"]
     
+    @State private var currentChoice: String = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
