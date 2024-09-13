c0082470c8e270b8640a71643cc0813ea56057de
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-13 11:54:42 +0200

    • r Substitute the .fontWeight for a .headline .font

--- 04-M-P01_03-RockPaperScissors/RockPaperScissors/ContentView.swift
+++ 04-M-P01_03-RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Computer has played...")
-                .fontWeight(.bold)
+                .font(.headline)
         }
         .padding()
     }
