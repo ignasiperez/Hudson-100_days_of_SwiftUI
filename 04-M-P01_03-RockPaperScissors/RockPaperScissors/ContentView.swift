4cb422a8f4489b52138e27621e8fc63d559dc614
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-16 11:54:26 +0200

    • r Substitute numberOfQuestions property (questionCounter)

--- 04-M-P01_03-RockPaperScissors/RockPaperScissors/ContentView.swift
+++ 04-M-P01_03-RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     @State private var score: Int = 0
     
-    @State private var numberOfQuestions = 0
+    @State private var questionCounter = 0
     
     var body: some View {
         VStack {
