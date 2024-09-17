9ad9cd1a75db5a63740dd290410bc75f3475feb1
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-17 12:21:36 +0200

    • f 01-L01 Create BetterRest folder in SwiftUI_Projects_Overviews project

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/BetterRest/Stepper_LocalizedStringKey_value.swift
@@ -0,0 +1,12 @@
+
+import SwiftUI
+
+struct Stepper_LocalizedStringKey_value: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Stepper_LocalizedStringKey_value()
+}
