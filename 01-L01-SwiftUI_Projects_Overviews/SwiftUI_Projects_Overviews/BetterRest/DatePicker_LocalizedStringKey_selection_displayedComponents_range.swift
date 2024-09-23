a48fbdadf563c8ea832d5f71058fd635c646294a
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-23 12:14:57 +0200

    • f Create a range from the current date up to tomorrow (let range)

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/BetterRest/DatePicker_LocalizedStringKey_selection_displayedComponents_range.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/BetterRest/DatePicker_LocalizedStringKey_selection_displayedComponents_range.swift
@@ -1,6 +1,7 @@
 //
 // §Date
 //      §addingTimeInterval(TimeInterval:)
+//      §now
 //
 
 import SwiftUI
@@ -20,6 +21,9 @@ struct DatePicker_LocalizedStringKey_selection_displayedComponents_range: View {
     func exampleDates() {
         // create a Date constant set to one day in seconds from now
         let tomorrow = Date.now.addingTimeInterval(86400)
+        
+        // create a range from the current date up to tomorrow
+        let range = Date.now...tomorrow
     }
 }
 
