a4641ec98153fe5508c3aabff85c2c9372c82a3d
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-24 11:51:02 +0200

    • f Create a §Date with a §Calendar through §DateComponents (let date)

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/BetterRest/Calendar_DateComponents.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/BetterRest/Calendar_DateComponents.swift
@@ -1,7 +1,9 @@
 //
-// §DateComponents
-//      §hour
-//      §minute
+// §DateComponents§hour
+// §DateComponents§minute
+//
+// §Calendar§current
+// §Calendar§date(from:)
 //
 
 import SwiftUI
@@ -15,7 +17,7 @@ struct Calendar_DateComponents: View {
         var components = DateComponents()
         components.hour = 8
         components.minute = 0
-        
+        let date = Calendar.current.date(from: components) ?? .now
     }
 }
 
