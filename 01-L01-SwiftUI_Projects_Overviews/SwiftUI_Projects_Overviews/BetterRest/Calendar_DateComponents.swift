7bcbd3e6cba8bea25002858b142a5ff3b03270b9
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-24 11:36:56 +0200

    • f Create a §DateComponents var (components)

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/BetterRest/Calendar_DateComponents.swift
@@ -0,0 +1,20 @@
+//
+
+//
+
+import SwiftUI
+
+struct Calendar_DateComponents: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+    
+    func ExampleDates() {
+        var components = DateComponents()
+        
+    }
+}
+
+#Preview {
+    Calendar_DateComponents()
+}
