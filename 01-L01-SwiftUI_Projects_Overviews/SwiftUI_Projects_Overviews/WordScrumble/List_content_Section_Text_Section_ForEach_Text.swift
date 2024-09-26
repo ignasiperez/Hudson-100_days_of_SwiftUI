0102737456aa37670d746540c269162590088f1f
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-26 17:25:22 +0200

    • f Combine the static and dynamic rows with sections (§Section)

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/WordScrumble/List_content_Section_Text_Section_ForEach_Text.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/WordScrumble/List_content_Section_Text_Section_ForEach_Text.swift
@@ -1,24 +1,31 @@
 //
 // §List(˚content:)
-//      Text
-//      ForEach
-//          Text
+//      §.listStyle(˚ListStyle:)
 //
 
 import SwiftUI
 
 struct List_content_Section_Text_Section_ForEach_Text: View {
     var body: some View {
-        Text("Static row 1")
-        Text("Static row 2")
-        
-        ForEach(0..<5) {
-            Text("Dynamic row \($0)")
-        }
-        
-        Text("Static row 3")
-        Text("Static row 4")    }
-}
+        List {
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+            
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+            
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
+        } // List
+    }
+} // List_content_Section_Text_Section_ForEach_Text
 
 #Preview {
     List_content_Section_Text_Section_ForEach_Text()
