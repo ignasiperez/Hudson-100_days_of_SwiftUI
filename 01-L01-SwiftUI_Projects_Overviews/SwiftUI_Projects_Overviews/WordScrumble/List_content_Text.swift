495a795baa8767b55c67626cac21968054c4807d
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-09-26 12:41:51 +0200

    • f Creates a §List with §Text view static content

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/WordScrumble/List_content_Text.swift
@@ -0,0 +1,20 @@
+//
+// §List(˚content:)
+//      Text
+//
+
+import SwiftUI
+
+struct List_content_Text: View {
+    var body: some View {
+        List {
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+        }
+    }
+}
+
+#Preview {
+    List_content_Text()
+}
