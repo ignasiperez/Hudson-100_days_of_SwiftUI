d8a678192004d78d07a6c052ed133fbac94700a2
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-03 13:04:57 +0200

    • f 05-P05 Create WordScrumble project

---
+++ 05-P05-WordScru﻿mble/WordScru﻿mble/WordScru_mbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScru_mbleApp.swift
+//  WordScru﻿mble
+//
+//  Created by Ignasi Perez-Valls on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScru_mbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
