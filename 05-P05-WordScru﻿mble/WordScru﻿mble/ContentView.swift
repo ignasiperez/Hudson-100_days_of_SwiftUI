59d159ca7c39bc60655f9d2ce72a203fc7c786a6
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-03 13:25:34 +0200

    • f Create an array of words they have already been used (var usedWords)

--- 05-P05-WordScru﻿mble/WordScru﻿mble/ContentView.swift
+++ 05-P05-WordScru﻿mble/WordScru﻿mble/ContentView.swift
@@ -2,6 +2,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
