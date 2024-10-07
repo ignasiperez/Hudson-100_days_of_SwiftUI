347b2c0549ea8439e0d8a417eb2e253ca0caaed5
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-07 11:43:30 +0200

    • f Use Apple’s SF Symbols icons to show the length of each word next to the text (§Image(˚systemName:)

--- 05-P05-WordScru﻿mble/WordScru﻿mble/ContentView.swift
+++ 05-P05-WordScru﻿mble/WordScru﻿mble/ContentView.swift
@@ -16,7 +16,10 @@ struct ContentView: View {
                 
                 Section {
                     ForEach(usedWords, id: \.self) { word in
-                        Text(word)
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
                     }
                 }
             }
