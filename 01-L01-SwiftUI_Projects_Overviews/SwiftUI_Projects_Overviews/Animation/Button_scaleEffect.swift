8b2efdda62d6cb49e3052e442009c488c66b0c21
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-14 17:41:13 +0200

    • f Create CGFloat @State property (var animationAmount = 1.0)

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_scaleEffect.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_scaleEffect.swift
@@ -5,6 +5,8 @@
 import SwiftUI
 
 struct Button_scaleEffect: View {
+    @State private var animationAmount: CGFloat = 1.0
+    
     var body: some View {
         Button("Tap Me") {
             // Do nothing
