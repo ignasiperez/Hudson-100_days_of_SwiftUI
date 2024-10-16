ca73bbfaeeb2c647d291aaf9886a33e2e745bb0d
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-16 13:03:18 +0200

    • f Copy & paste "Creating implicit animations"

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_animation.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_animation.swift
@@ -8,6 +8,7 @@ struct Button_animation: View {
     @State private var animationAmount: CGFloat = 1.0
     @State private var animationAmount2: CGFloat = 1.0
     @State private var animationAmount3: CGFloat = 1.0
+    @State private var animationAmount4: CGFloat = 1.0
     
     var body: some View {
         VStack {
@@ -52,8 +53,20 @@ struct Button_animation: View {
                        value: animationAmount3)
             
             Spacer()
+            
+            Button("Tap ±easeInOut") {
+                animationAmount4 += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount4)
+            .blur(radius: (animationAmount4 - 1) * 3)
+            .animation(.default, value: animationAmount4)
+            
+            Spacer()
         }
-        
     }
 }
 
