91ed6dbc3fdbf2a9aeb0aa5c1d8ce820b0d9cf3b
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-16 18:28:57 +0200

    • f Create CGFloat @State property (var animationAmount = 1.0)

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_onAppear_overlay_Circle_repeatForever.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_onAppear_overlay_Circle_repeatForever.swift
@@ -5,6 +5,8 @@
 import SwiftUI
 
 struct Button_onAppear_overlay_Circle_repeatForever: View {
+    @State var animationAmount: CGFloat = 1.0
+    
     var body: some View {
         Button("Tap me") {
             
