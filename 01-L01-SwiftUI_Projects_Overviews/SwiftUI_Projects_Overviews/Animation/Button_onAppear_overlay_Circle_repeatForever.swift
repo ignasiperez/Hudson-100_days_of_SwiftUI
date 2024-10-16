a33ff287cda97c391e10faa7a8c3608af22e2b52
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-16 18:32:58 +0200

    • f Add §.onAppear() to the button, which sets animationAmount to 2

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_onAppear_overlay_Circle_repeatForever.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_onAppear_overlay_Circle_repeatForever.swift
@@ -15,6 +15,9 @@ struct Button_onAppear_overlay_Circle_repeatForever: View {
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
