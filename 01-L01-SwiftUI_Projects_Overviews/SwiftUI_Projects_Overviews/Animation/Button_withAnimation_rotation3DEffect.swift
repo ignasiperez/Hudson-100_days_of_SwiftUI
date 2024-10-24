28f75bc209f1eeea9b17f093e2565d29071c3b76
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-24 11:05:28 +0200

    • o  Increase the animation amount by 360 when the button is tapped

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_withAnimation_rotation3DEffect.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/Button_withAnimation_rotation3DEffect.swift
@@ -9,7 +9,7 @@ struct Button_withAnimation_rotation3DEffect: View {
     
     var body: some View {
         Button("Tap me") {
-            // Do nothing
+            animationAmount += 360
         }
         .padding(50)
         .background(.red)
