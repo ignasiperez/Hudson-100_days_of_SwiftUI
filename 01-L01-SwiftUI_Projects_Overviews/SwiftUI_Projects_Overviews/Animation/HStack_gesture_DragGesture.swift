d022ba02543fd6128bc89c026128543eef2d86e3
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-29 13:21:44 +0100

    • o Create a @State property (var enabled = false)

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/HStack_gesture_DragGesture.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/HStack_gesture_DragGesture.swift
@@ -6,6 +6,7 @@ import SwiftUI
 
 struct HStack_gesture_DragGesture: View {
     let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
