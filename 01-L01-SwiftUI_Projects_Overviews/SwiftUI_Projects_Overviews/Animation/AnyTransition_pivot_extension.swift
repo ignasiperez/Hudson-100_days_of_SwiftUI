a11366fbfd48f6fbd76adb265c1eac8c926b78c9
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-30 17:19:04 +0100

    • o Create a ZStack that contains a §Rectangle §.fill with ±blue and a 200x200 §.frame

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/AnyTransition_pivot_extension.swift
@@ -0,0 +1,19 @@
+//
+//
+//
+
+import SwiftUI
+
+struct AnyTransition_pivot_extension: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 200, height: 200)
+        }
+    }
+}
+
+#Preview {
+    AnyTransition_pivot_extension()
+}
