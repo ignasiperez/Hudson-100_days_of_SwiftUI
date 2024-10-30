65f72127817db1d76745953b058a5d2f738d5eaa
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-10-30 14:46:15 +0100

    • o Add an ±asymmetric §.transition to a second rectangle with ±scale ˚insertion and ±opacity ˚removal

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/VStack_Button_withAnimation.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/Animation/VStack_Button_withAnimation.swift
@@ -3,15 +3,19 @@
 //
 // §rectangle§.transition(˚AnyTransition)
 // §rectangle§.transition(±scale)
+// §rectangle§.transition(±asymmetric(insertion:removal:))
 //
 
 import SwiftUI
 
 struct VStack_Button_withAnimation: View {
     @State private var isShowingRed = false
+    @State private var isShowingRed2 = false
     
     var body: some View {
         VStack {
+            Spacer()
+            
             Button("Tap me ±scale §.transition") {
                 withAnimation {
                     isShowingRed.toggle()
@@ -24,6 +28,25 @@ struct VStack_Button_withAnimation: View {
                     .frame(width: 200, height: 200)
                     .transition(.scale)
             }
+            
+            Spacer()
+            
+            Button("Tap me ±scale §.asymetric") {
+                withAnimation {
+                    isShowingRed2.toggle()
+                }
+            }
+            
+            if isShowingRed2 {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(
+                        .asymmetric(insertion: .scale, removal: .opacity)
+                    )
+            }
+            
+            Spacer()
         }
     }
 }
