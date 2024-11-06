0a71458d961b4dcdd6ad46bb783f2fd9faa77d50
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-11-06 17:41:18 +0100

    • o Create a simple view, that contains a Text view, we want to show inside a sheet (SecondView) ("Second View")

---
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/iExpense/SecondView_ContentView_Button_sheet.swift
@@ -0,0 +1,21 @@
+//
+//
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Second View")
+    }
+}
+
+struct SecondView_ContentView_Button_sheet: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SecondView_ContentView_Button_sheet()
+}
