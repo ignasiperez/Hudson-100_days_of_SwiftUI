b1dbcac9ca0e2abd8884dbb5895e240b5f118830
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-11-11 14:07:37 +0100

    • o Create a @State property to store the current number (var currentNumber = 1)

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/iExpense/List_ForEach_onDelete.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/iExpense/List_ForEach_onDelete.swift
@@ -6,6 +6,7 @@ import SwiftUI
 
 struct List_ForEach_onDelete: View {
     @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
         Text("Hello, World!")
