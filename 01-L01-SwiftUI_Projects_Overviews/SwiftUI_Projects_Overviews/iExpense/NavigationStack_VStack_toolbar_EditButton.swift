7109771140fecbf26dfef29a4e2e5fe485f56d5a
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-11-13 12:35:15 +0100

    • o Wrap the VStack with a §NavigationStack

--- 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/iExpense/NavigationStack_VStack_toolbar_EditButton.swift
+++ 01-L01-SwiftUI_Projects_Overviews/SwiftUI_Projects_Overviews/iExpense/NavigationStack_VStack_toolbar_EditButton.swift
@@ -9,19 +9,20 @@ struct NavigationStack_VStack_toolbar_EditButton: View {
     @State private var currentNumber = 1
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(numbers, id: \.self) { number in
-                    Text("Row \(number)")
+         NavigationStack { 
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) { number in
+                        Text("Row \(number)")
+                    }
+                    .onDelete(perform: removeRows)
                 }
-                .onDelete(perform: removeRows)
+                
+                Button("Add number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
             }
-            
-        }
-        
-        Button("Add number") {
-            numbers.append(currentNumber)
-            currentNumber += 1
         }
     }
     
