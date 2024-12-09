836efa9d27438f842a2d303fe4f8a984eca9d1b6
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-12-09 14:17:03 +0100

    • f 07-P06 Create iExpense project

---
+++ 07-P06-iExpense/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ignasi Perez-Valls on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
